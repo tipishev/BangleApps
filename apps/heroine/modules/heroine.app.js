16a2162695f365e9f80bfa39622b9659112264de
Timofei Tipishev <tipishev@gmail.com>
2022-10-30 00:59:43 +0200

    make a catchall context

---
+++ apps/heroine/modules/heroine.app.js
@@ -0,0 +1,72 @@
+/* Main Heroine Dusk module */
+
+
+// load and draw widgets
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+
+// load modules
+
+var avatar_m = require("heroine_avatar");
+var gamestate_m = require("heroine_gamestate");
+var explore_m = require("heroine_explore");
+var mazemap_m = require("heroine_mazemap");
+
+// declare objects
+
+var avatar, explore, gamestate, mazemap;
+
+function export_context() {
+  var ctx = {};
+  ctx.avatar = avatar;
+  ctx.gamestate = gamestate;
+  ctx.explore = explore;
+  ctx.mazemap = mazemap;
+  return ctx;
+}
+
+function import_context(ctx) {
+  avatar = ctx.avatar;
+  gamestate = ctx.gamestate;
+  explore = ctx.explore;
+  mazemap = ctx.mazemap;
+}
+
+// initialize objects
+
+avatar = avatar_m.init();
+explore = explore_m.init();
+gamestate = gamestate_m.init();
+mazemap = mazemap_m.init();
+
+//mazemap_m.render(mazemap, avatar.x, avatar.y, avatar.facing);
+
+// controls
+// TODO move to a separate module
+Bangle.on("swipe",
+    function(directionLR, directionUD){
+    Bangle.buzz(50);
+    var input = {};
+    if (directionLR === 0 && directionUD === -1) {
+      input.up = true;
+    } else if (directionLR === -1 && directionUD === 0) {
+      input.left = true;
+    } else if (directionLR === +1 && directionUD === 0) {
+      input.right = true;
+    } else if (directionLR === 0 && directionUD === 1) {
+      input.down = true;
+    }
+
+    var logic_ctx = export_context();
+    logic_ctx.input = input;
+
+    var logic_result = gamestate_m.logic(logic_ctx);
+    import_context(logic_result);
+
+
+    if (logic_result.redraw) {
+      gamestate_m.render(export_context());
+      Bangle.buzz(100);  // half-an-hour later...
+    }
+  });
