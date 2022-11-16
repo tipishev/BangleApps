95695b3a5b9ddff189e99a3b4e8e2fb23fe8bbc8
Timofei Tipishev <tipishev@gmail.com>
2022-11-17 00:01:43 +0100

    draw cursor as a triangle

--- apps/heroine/minimap.js
+++ apps/heroine/minimap.js
@@ -4,68 +4,68 @@
  */
 
 // FIXME wrap constants in a function
+const heatshrink_m = require("heatshrink");
 const atlas_m = require("heroine_atlas");
 const tileset_m = require("heroine_tileset");
 
 const ICON_SIZE = 3; // pixels
 
-const MARGIN_LEFT = 2;
-const MARGIN_TOP = 2;
+// explained by the position of game canvas offset
+// TODO deduplicate with tileset.js constants
+const GLOBAL_MARGIN_LEFT = 8;
+const GLOBAL_MARGIN_TOP = 32;
+
+const MARGIN_LEFT = GLOBAL_MARGIN_LEFT + 2;
+const MARGIN_TOP = GLOBAL_MARGIN_TOP + 2;
 
 // TODO de-atob-ize, store only strings.
-const ICON_NONWALKABLE = require("heatshrink").decompress(atob("gcDwcBA4YA=="));
-const ICON_EXIT = require("heatshrink").decompress(atob("gcDwcAkmSpMg"));
-const ICON_WALKABLE = require("heatshrink").decompress(atob("gcDwcA/4AB8A"));
 
-const CURSOR_SOUTH = require("heatshrink").decompress(atob("gcDwcAyV54kA"));
-const CURSOR_WEST = require("heatshrink").decompress(atob("gcDwcAj1Jg+A"));
-const CURSOR_EAST = require("heatshrink").decompress(atob("gcDwcAzlJk8A"));
-const CURSOR_NORTH = require("heatshrink").decompress(atob("gcDwcAiF58mA"));
+const CURSOR_SOUTH = heatshrink_m.decompress(atob("gcDwcAyV54kA"));
+const CURSOR_WEST = heatshrink_m.decompress(atob("gcDwcAj1Jg+A"));
+const CURSOR_EAST = heatshrink_m.decompress(atob("gcDwcAzlJk8A"));
+const CURSOR_NORTH = heatshrink_m.decompress(atob("gcDwcAiF58mA"));
 
 // exports
 
 exports.render = function(ctx) {
-  var mazemap = ctx.mazemap;
-  var avatar = ctx.avatar;
+  const mazemap = ctx.mazemap;
+  const avatar = ctx.avatar;
 
-  var atlas = atlas_m.atlas();
+  const BLACK = "#000";
+  const WHITE = "#FFF";
+  const BLUE = "#00F";
+  const RED = "#F00";
 
-  // starting draw location
-  var left_x = MARGIN_LEFT;
-  var top_y = MARGIN_TOP;
+  const atlas = atlas_m.atlas();
 
-  var draw_x;
-  var draw_y;
+  let x;
+  let y;
   var target_tile;
 
   // render map
-  for (var i=0; i<mazemap.width; i++) {
-    draw_x = i * ICON_SIZE + left_x;
-
-    for (var j=0; j<mazemap.height; j++) {
-	  draw_y = j * ICON_SIZE + top_y;
-
-      target_tile = mazemap_m.get_tile(mazemap, i, j);
-      if (tileset_m.is_walkable[target_tile]) {
-	    render_icon(ICON_WALKABLE, draw_x, draw_y);
+  for (let x=0; x<mazemap.width; x++) {
+    for (let y=0; y<mazemap.height; y++) {
+      target_tile = mazemap_m.get_tile(mazemap, x, y);
+      let color;
+      if (tileset_m.is_walkable(target_tile)) {
+        color = WHITE;
 	  }
       else if (target_tile != 0) {
-        render_icon(ICON_NONWALKABLE, draw_x, draw_y);
+        color = BLACK;
       }
+      render_square(x, y, color);
 	}
   }
-/*
-  // render exits
-  var exit_x;
-  var exit_y;
 
-  for (var i=0; i<atlas.maps[mazemap.current_id].exits.length; i++) {
+
+  // render exits
+  for (let i=0; i<atlas.maps[mazemap.current_id].exits.length; i++) {
     exit_x = atlas.maps[mazemap.current_id].exits[i].exit_x;
 	exit_y = atlas.maps[mazemap.current_id].exits[i].exit_y;
-	draw_x = exit_x * ICON_SIZE + left_x;
-	draw_y = exit_y * ICON_SIZE + top_y;
-	render_icon(draw_x, draw_y, ICON_EXIT);
+    render_square(exit_x, exit_y, BLUE);
   }
+
+  /*
 
   // render shops
   for (var i=0; i<atlas.maps[mazemap.current_id].shops.length; i++) {
@@ -75,23 +75,35 @@ exports.render = function(ctx) {
 	draw_y = exit_y * ICON_SIZE + top_y;
 	render_icon(draw_x, draw_y, ICON_EXIT);
   }
+  */
 
   // render avatar cursor
-  draw_x = avatar.x * ICON_SIZE + left_x;
-  draw_y = avatar.y * ICON_SIZE + top_y;
-  var cursor_direction;
-  if (avatar.facing == "west") cursor_direction = CURSOR_WEST;
-  else if (avatar.facing == "north") cursor_direction = CURSOR_NORTH;
-  else if (avatar.facing == "east") cursor_direction = CURSOR_EAST;
-  else if (avatar.facing == "south") cursor_direction = CURSOR_SOUTH;
-  render_cursor(draw_x, draw_y, cursor_direction);
-  */
+  //var cursor_direction;
+  //if (avatar.facing == "west") cursor_direction = CURSOR_WEST;
+  //else if (avatar.facing == "north") cursor_direction = CURSOR_NORTH;
+  //else if (avatar.facing == "east") cursor_direction = CURSOR_EAST;
+  //else if (avatar.facing == "south") cursor_direction = CURSOR_SOUTH;
+  //render_cursor(draw_x, draw_y, cursor_direction);
+  render_cursor(avatar.x, avatar.y, avatar.drection, RED);
 
 };
 
-function render_icon(icon, screen_x, screen_y) {
-  g.drawImage(icon, MARGIN_LEFT + screen_x + ICON_SIZE, MARGIN_TOP + screen_y + ICON_SIZE);
+// x and y are logical coordinate, e.g. (0, 0)
+function render_square(x, y, color) {
+  g.setColor(color);
+  g.fillRect(MARGIN_LEFT + x * (ICON_SIZE + 1),
+             MARGIN_TOP + y * (ICON_SIZE + 1),
+             MARGIN_LEFT + x * (ICON_SIZE + 1) + ICON_SIZE,
+             MARGIN_TOP + y * (ICON_SIZE + 1) + ICON_SIZE);
 }
+
+function render_cursor(x, y, direction, color) {
+  g.setColor(color);
+  g.fillPoly([MARGIN_LEFT + x * (ICON_SIZE + 1), MARGIN_TOP + y * (ICON_SIZE + 1),
+             MARGIN_LEFT + x * (ICON_SIZE + 1) + ICON_SIZE, MARGIN_TOP + y * (ICON_SIZE + 1),
+             MARGIN_LEFT + x * (ICON_SIZE + 1) + ICON_SIZE / 2, MARGIN_TOP + y * (ICON_SIZE + 1) + ICON_SIZE]);
+}
+
 
 /*
 function render_cursor(screen_x, screen_y, cursor_dir) {
